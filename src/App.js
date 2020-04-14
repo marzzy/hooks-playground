3eadc1a1f545c104a6d67b30e701f669457e3a56
marzzy mogh <marzzy.m@gmail.com>
2020-04-14 21:00:57 +0430

    faat(router): add simple navigation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import React from 'react';
-// import Counter from './Components/Counter'
-// import UserName from './Components/UserName';
-// import Calc from './Components/Calc';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Counter from './Components/Counter';
+import UserName from './Components/UserName';
+import Calc from './Components/Calc';
 import TodoList from './Components/TodoList';
+import Home from './Components/Home';
 
-function App() {
+export default function BasicNavigation() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        hello world
-      </header>
-      <br /> */}
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calc">calc</Link>
+          </li>
+          <li>
+            <Link to="/counter">counter</Link>
+          </li>
+          <li>
+            <Link to="/userName">userName</Link>
+          </li>
+          <li>
+            <Link to="/todo">todo</Link>
+          </li>
+        </ul>
 
-      {/* <Counter />
-      <br/> */}
+        <hr />
 
-      {/* <UserName />
-      <br /> */}
-
-      <TodoList />
-      <br />
-
-      {/* <Calc />
-      <br/> */}
-
-    </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/calc">
+            <Calc />
+          </Route>
+          <Route path="/counter">
+            <Counter />
+          </Route>
+          <Route path="/userName">
+            <UserName />
+          </Route>
+          <Route path="/todo">
+            <TodoList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hi
+      </h1>
+      Plz choose one of the route above
+    </div>
+  );
+}
