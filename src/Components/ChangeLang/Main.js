ee1b0a247d524ef06b7761fb7dc75e778fdc966c
marzzy mogh <marzzy.m@gmail.com>
2020-04-15 16:48:36 +0430

    feat(language-example): change lang button way 01

--- src/Components/ChangeLang/Main.js
+++ src/Components/ChangeLang/Main.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Others from './Others';
 import { LangContext, languagesObject } from './langContext';
 
 export default function MainBiggerComponent() {
+  const [lang, setLang] = useState('fa');
+
+  function toggleLang() {
+    setLang(lang === 'en' ? 'fa' : 'en');
+  }
+
   return (
-    <LangContext.Provider value={languagesObject.fa}>
+    <LangContext.Provider value={languagesObject[lang]}>
       <Others />
+      <button type="submit" onClick={toggleLang}>
+        active lang :
+        {lang}
+      </button>
     </LangContext.Provider>
   );
 }
